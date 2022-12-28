1123c8a15fae590730c3772f884e65433a81b871
Rolls98 <toureb16@gmail.com>
2022-12-28 22:05:51 +0000

    multer storage destination

--- routes/index.js
+++ routes/index.js
@@ -36,9 +36,7 @@ const multer = require('multer');
 const { produitQueries } = require('../requests/produitQueries');
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
-  },
+  destination: './uploads/',
   filename: function (req, file, cb) {
     console.log(file);
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
