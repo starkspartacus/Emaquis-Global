2938dbd0baad8df318f28a2dccd9aceec17bb944
Aniki <yohaneaniki@gmail.com>
2023-01-31 16:53:44 +0000

    Update commandecontroller.js

--- controllers/commandecontroller.js
+++ controllers/commandecontroller.js
@@ -5,28 +5,29 @@ exports.commande = async (req, res) => {
     const travail_pour = req.body.session || req.session.user.travail_pour;
 
     const employeId = req.body.employe || req.session.user._id;
-    const commandes = [];
+  
     const vente = await venteQueries.getVentes({
       travail_pour: travail_pour,
       employe: employeId,
       status_commande: "En attente",
+      
     });
 
     if (vente) {
-     
-      vente.result.forEach(commande => {
-        commandes.push({
-          nom: commande.produit[0].produit.nom_produit,
-          quantité: commande.quantite[0],
-          prix: commande.prix,
-          monnaie: commande.monnaie,
-          somme_encaissée: commande.somme_encaisse,
-          employé: `${commande.employe.prenom} ${commande.employe.nom}`,
-          "statut de la commande":"En attende"
+        const commandes = vente.result.map(commande => {
+          return {
+            produits: commande.produit.map(prod => prod.produit.nom_produit),
+            quantité: commande.quantite,
+            monnaie: commande.monnaie,
+            somme_encaissée: commande.somme_encaisse,
+            employé: `${commande.employe.prenom} ${commande.employe.nom}`,
+            prix: commande.prix,
+          };
         });
-      });
-      res.json(commandes);
-    }
+        
+        res.json(commandes);
+        
+      }
   } catch (e) {
     res.json({
       etat: false,
