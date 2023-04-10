dd806190a7bfb4ce103e111482da314b293d2274
LITIE <69165249+starkspartacus@users.noreply.github.com>
2023-04-10 15:08:03 +0200

    update size locker 100

--- utils/generateQuantityByLocker.js
+++ utils/generateQuantityByLocker.js
@@ -4,10 +4,10 @@ exports.generateQuantityByLocker = (locker, size) => {
 
   if (['50cl', '60cl', '65cl'].includes(size)) {
     return locker * 12;
-  } else if (['30cl', '33cl'].includes(size)) {
+  } else if (['30cl', '33cl', '25cl'].includes(size)) {
     return locker * 24;
   } else if (['100cl'].includes(size)) {
-    return locker * 100;
+    return locker * 12;
   }
 
   return 12;
