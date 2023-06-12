a0f3416a88aa3d6338a3ef9a0139ada6c7aad436
starkspartacus <133286746+hnm7877@users.noreply.github.com>
2023-06-12 04:48:44 +0200

    etat connexion

--- public/js/qualityConnexion.js
+++ public/js/qualityConnexion.js
@@ -1,3 +1,5 @@
+const qualityConnexion = document.getElementById('qualityConnexion');
+
 function checkInternetConnection() {
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
@@ -6,35 +8,46 @@ function checkInternetConnection() {
         xhr.open('GET', 'http://localhost:3000/', true); // URL pour le test de connexion
 
         xhr.onload = function() {
-            if (xhr.status === 200 && timeout <= 5000) {
-                resolve(true); // La connexion a réussi
+            if (xhr.status === 200 && xhr.timeout <= 5000) {
+                resolve({ isConnected: true, timeout: xhr.timeout }); // La connexion a réussi
             } else {
-                resolve(false); // La connexion a échoué
+                resolve({ isConnected: true, timeout: xhr.timeout > 5000 }); // La connexion a échoué ou est lente
             }
         };
 
+
+
         xhr.onerror = function() {
-            resolve(false); // La connexion a échoué
+            resolve({ isConnected: false, timeout: xhr.timeout }); // La connexion a échoué
         };
 
         xhr.timeout = timeout;
         xhr.ontimeout = function() {
-            resolve(false); // La connexion a expiré
+            resolve({ isConnected: false, timeout: xhr.timeout }); // La connexion a expiré
         };
 
         xhr.send();
     });
 }
 
-// Exemple d'utilisation
-checkInternetConnection()
-    .then((isConnected) => {
-        if (isConnected) {
-            console.log("Vous êtes connecté à Internet.");
-        } else {
-            console.log("Vous n'êtes pas connecté à Internet.");
-        }
-    })
-    .catch((error) => {
-        console.error("Une erreur s'est produite :", error);
-    });
+//  d'utilisation
+document.addEventListener('DOMContentLoaded', function() {
+    checkInternetConnection()
+        .then((result) => {
+
+            if (result.isConnected) {
+                qualityConnexion.classList.add('quality_Connexion');
+                qualityConnexion.classList.remove('quality_Connexion_lente');
+            } else  {
+                qualityConnexion.classList.remove('quality_Connexion');
+                qualityConnexion.classList.add('quality_Connexion_lente');
+
+            }
+        })
+        .catch((error) => {
+            console.error("Une erreur s'est produite :", error);
+        });
+});
+
+
+
