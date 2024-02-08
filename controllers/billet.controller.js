c8b07ec77ab52933fe506f86480a93c49a871a74
Rolls98 <toureb16@gmail.com>
2024-02-08 22:54:56 +0000

    fix return product with promo

--- controllers/billet.controller.js
+++ controllers/billet.controller.js
@@ -3,141 +3,144 @@ const { BilletQueries } = require('../requests/BilletQueries');
 const { returnStock } = require('./stock.controller');
 
 exports.getOneBillet = async (req, res) => {
-  if (req.session.user) {
-    try {
-      const billet = await BilletQueries.getBilletByEmployeId(req.params.id);
-      if (billet.result) {
-        res.json({
-          etat: true,
-          data: billet.result,
-        });
-      } else {
-        res.json({
-          etat: false,
-          message: 'billet not found',
-        });
-      }
-    } catch (error) {
-      res.json({
-        etat: false,
-        message: error.message,
-      });
-    }
-  } else {
-    res.status(401).send({
-      error: 'error signature',
-    });
-  }
+	if (req.session.user) {
+		try {
+			const billet = await BilletQueries.getBilletByEmployeId(req.params.id);
+			if (billet.result) {
+				res.json({
+					etat: true,
+					data: billet.result,
+				});
+			} else {
+				res.json({
+					etat: false,
+					message: 'billet not found',
+				});
+			}
+		} catch (error) {
+			res.json({
+				etat: false,
+				message: error.message,
+			});
+		}
+	} else {
+		res.status(401).send({
+			error: 'error signature',
+		});
+	}
 };
 
 exports.openBillet = async (req, res) => {
-  if (req.session.user) {
-    try {
-      const body = req.body;
+	if (req.session.user) {
+		try {
+			const body = req.body;
 
-      let billet = await BilletQueries.getBilletByEmployeId(body.employe_id);
+			let billet = await BilletQueries.getBilletByEmployeId(body.employe_id);
 
-      if (!billet.result) {
-        billet = await BilletQueries.setBillet({
-          employe_id: body.employe_id,
-          open_hour: new Date(),
-          travail_pour: req.session.user.travail_pour,
-        });
-      }
+			if (!billet.result) {
+				billet = await BilletQueries.setBillet({
+					employe_id: body.employe_id,
+					open_hour: new Date(),
+					travail_pour: req.session.user.travail_pour,
+				});
+			}
 
-      if (billet.etat) {
-        const productsReturn = await returnStock(req);
-        res.json({
-          etat: true,
-          data: billet.result,
-          productsReturn,
-        });
-      }
-    } catch (error) {
-      res.json({
-        etat: false,
-        message: error.message,
-      });
-    }
-  } else {
-    res.status(401).send({
-      error: 'error signature',
-    });
-  }
+			if (billet.etat) {
+				const productsReturn = await returnStock(req);
+				res.json({
+					etat: true,
+					data: billet.result,
+					productsReturn,
+				});
+			}
+		} catch (error) {
+			res.json({
+				etat: false,
+				message: error.message,
+			});
+		}
+	} else {
+		res.status(401).send({
+			error: 'error signature',
+		});
+	}
 };
 
 exports.closeBillet = async (req, res) => {
-  if (req.session.user) {
-    try {
-      const body = req.body;
+	if (req.session.user) {
+		try {
+			const body = req.body;
 
-      let billetRes = await BilletQueries.getBilletByEmployeId(body.employe_id);
+			let billetRes = await BilletQueries.getBilletByEmployeId(body.employe_id);
 
-      if (billetRes.result) {
-        const billet = billetRes.result;
+			if (billetRes.result) {
+				const billet = billetRes.result;
 
-        const commonProperties = {
-          for_employe: body.employe_id,
-          createdAt: {
-            $gte: new Date(new Date(billet.open_hour)),
-            $lte: new Date(),
-          },
-        };
-        const ventes = await venteModel.find({
-          ...commonProperties,
-          status_commande: 'En attente',
-        });
+				const commonProperties = {
+					for_employe: body.employe_id,
+					createdAt: {
+						$gte: new Date(new Date(billet.open_hour)),
+						$lte: new Date(),
+					},
+				};
+				const ventes = await venteModel.find({
+					...commonProperties,
+					status_commande: 'En attente',
+				});
 
-        if (ventes && ventes?.length > 0) {
-          res.status(400).send({
-            message: `Vous avez ${ventes.length} commandes en attente, veuillez les valider avant de fermer la caisse. (Voir la liste des commandes en attente)`,
-          });
-        } else {
-          billet.is_closed = true;
-          billet.close_hour = new Date();
+				if (
+					ventes &&
+					ventes.filter((vente) => vente.travail_pour)?.length > 0
+				) {
+					res.status(400).send({
+						message: `Vous avez ${ventes.length} commandes en attente, veuillez les valider avant de fermer la caisse. (Voir la liste des commandes en attente)`,
+					});
+				} else {
+					billet.is_closed = true;
+					billet.close_hour = new Date();
 
-          const ventesConfirme = await venteModel.find({
-            ...commonProperties,
-            employe_validate_id: body.employe_id,
-            status_commande: 'Validée',
-          });
-          const ventesAnnule = await venteModel.find({
-            ...commonProperties,
-            employe_validate_id: body.employe_id,
-            status_commande: 'Annulée',
-          });
+					const ventesConfirme = await venteModel.find({
+						...commonProperties,
+						employe_validate_id: body.employe_id,
+						status_commande: 'Validée',
+					});
+					const ventesAnnule = await venteModel.find({
+						...commonProperties,
+						employe_validate_id: body.employe_id,
+						status_commande: 'Annulée',
+					});
 
-          billet.total_order_validated = ventesConfirme.length;
-          billet.total_order_canceled = ventesAnnule.length;
-          billet.total_order = ventesConfirme.length + ventesAnnule.length;
-          billet.total_amount = ventesConfirme.reduce(
-            (acc, vente) => acc + vente.prix,
-            0
-          );
+					billet.total_order_validated = ventesConfirme.length;
+					billet.total_order_canceled = ventesAnnule.length;
+					billet.total_order = ventesConfirme.length + ventesAnnule.length;
+					billet.total_amount = ventesConfirme.reduce(
+						(acc, vente) => acc + vente.prix,
+						0
+					);
 
-          const { _id, ...billetData } = billet._doc;
+					const { _id, ...billetData } = billet._doc;
 
-          await BilletQueries.updateBilletById(_id, billetData);
+					await BilletQueries.updateBilletById(_id, billetData);
 
-          res.json({
-            etat: true,
-            data: billet,
-          });
-        }
-      } else {
-        res.status(404).send({
-          error: 'billet not found',
-        });
-      }
-    } catch (error) {
-      res.json({
-        etat: false,
-        message: error.message,
-      });
-    }
-  } else {
-    res.status(401).send({
-      error: 'error signature',
-    });
-  }
+					res.json({
+						etat: true,
+						data: billet,
+					});
+				}
+			} else {
+				res.status(404).send({
+					error: 'billet not found',
+				});
+			}
+		} catch (error) {
+			res.json({
+				etat: false,
+				message: error.message,
+			});
+		}
+	} else {
+		res.status(401).send({
+			error: 'error signature',
+		});
+	}
 };
--- controllers/retourproduit.js
+++ controllers/retourproduit.js
@@ -6,337 +6,349 @@ const { helperConverStrToArr } = require('../utils/helperConvertStrToArr');
 const { TYPE_RETOUR_PRDUITS } = require('../constants');
 const { venteQueries } = require('../requests/venteQueries');
 const {
-  helperFormatReturnProducts,
+	helperFormatReturnProducts,
 } = require('../utils/helperFormatReturnProducts');
 const { formatQuery } = require('../utils/formatQuery');
 
 exports.addback = async (req, res) => {
-  try {
-    if (req.session.user) {
-      const userSession = req.session.user.travail_pour;
+	try {
+		if (req.session.user) {
+			const userSession = req.session.user.travail_pour;
 
-      const { result: products } = await produitQueries.getProduitBySession(
-        userSession
-      );
+			const { result: products } = await produitQueries.getProduitBySession(
+				userSession
+			);
 
-      let { result: setting } = await settingQueries.getSettingByUserId(
-        userSession
-      );
+			let { result: setting } = await settingQueries.getSettingByUserId(
+				userSession
+			);
 
-      if (!setting) {
-        let res = await settingQueries.setSetting({
-          travail_pour: userSession,
-        });
+			if (!setting) {
+				let res = await settingQueries.setSetting({
+					travail_pour: userSession,
+				});
 
-        if (res.etat) {
-          setting = res.result;
-        }
-      }
+				if (res.etat) {
+					setting = res.result;
+				}
+			}
 
-      let retournData = null;
+			let retournData = null;
 
-      if (req.session.retournData) {
-        retournData = req.session.retournData;
-        delete req.session.retournData;
-      }
+			if (req.session.retournData) {
+				retournData = req.session.retournData;
+				delete req.session.retournData;
+			}
 
-      const return_product_label = TYPE_RETOUR_PRDUITS.find(
-        (item) => item.type === setting.product_return_type
-      )?.nom;
+			const return_product_label = TYPE_RETOUR_PRDUITS.find(
+				(item) => item.type === setting.product_return_type
+			)?.nom;
 
-      res.render('retour', {
-        user: req.session.user,
-        products,
-        setting,
-        return_product_label,
-        retournData,
-        code: retournData?._id?.slice(-6).toUpperCase(),
-      });
-    } else {
-      res.redirect('emconnexion');
-    }
-  } catch (error) {
-    res.redirect(error);
-  }
+			res.render('retour', {
+				user: req.session.user,
+				products,
+				setting,
+				return_product_label,
+				retournData,
+				code: retournData?._id?.slice(-6).toUpperCase(),
+			});
+		} else {
+			res.redirect('emconnexion');
+		}
+	} catch (error) {
+		res.redirect(error);
+	}
 };
 
 exports.addbackPost = async (req, res) => {
-  try {
-    const body = {
-      ...req.body,
-      produit: helperConverStrToArr(req.body.produit),
-      quantite: helperConverStrToArr(req.body.quantite),
-    };
-    const user = req.session.user;
+	try {
+		const body = {
+			...req.body,
+			produit: helperConverStrToArr(req.body.produit),
+			quantite: helperConverStrToArr(req.body.quantite),
+		};
+		console.log(body);
+		const user = req.session.user;
 
-    /// 1- remboursement total(on rembourse tout)(on recalcul le prix de vente avec le stock)
-    //  2- remboursement partiel(on rembourse partiellement)(on deduire la somme dans la caisse)
-    //  3- remboursement par avoir(ces boisons deviennent directement des pourboires)
+		/// 1- remboursement total(on rembourse tout)(on recalcul le prix de vente avec le stock)
+		//  2- remboursement partiel(on rembourse partiellement)(on deduire la somme dans la caisse)
+		//  3- remboursement par avoir(ces boisons deviennent directement des pourboires)
 
-    if (user) {
-      const { result: setting } = await settingQueries.getSettingByUserId(
-        user.travail_pour
-      );
+		if (user) {
+			const { result: setting } = await settingQueries.getSettingByUserId(
+				user.travail_pour
+			);
 
-      const product_return_type = setting.product_return_type;
+			const product_return_type = setting.product_return_type;
 
-      let total = 0;
+			let total = 0;
 
-      const updateProductStock = async () => {
-        let sum = 0;
-        for (let [index, produit_id] of body.produit.entries()) {
-          const reqData = {
-            _id: produit_id,
-            session: user.travail_pour,
-          };
+			const updateProductStock = async () => {
+				let sum = 0;
+				for (let [index, produit_id] of body.produit.entries()) {
+					const reqData = {
+						_id: produit_id,
+						session: user.travail_pour,
+					};
 
-          const produit = await Produits.findOne(reqData);
+					const produit = await Produits.findOne(reqData);
+					const quantite = Number(body.quantite[index]);
 
-          if (produit) {
-            produit.quantite += parseInt(body.quantite[index]);
-            sum += produit.prix_vente * parseInt(body.quantite[index]);
-            await produit.save();
-            body.produit[index] = {
-              ...produit._doc,
-              productId: produit._id,
-            };
-          }
-        }
+					if (produit) {
+						produit.quantite += parseInt(quantite);
 
-        return sum;
-      };
+						if (produit.promo && produit.promo_quantity <= quantite) {
+							let promo_quantity = parseInt(quantite / produit.promo_quantity);
 
-      if (['full', 'half'].includes(product_return_type)) {
-        total = await updateProductStock();
+							sum +=
+								promo_quantity * produit.promo_price +
+								(quantite % produit.promo_quantity) * produit.prix_vente;
+						} else {
+							sum += produit.prix_vente * quantite;
+						}
 
-        let venteData = {
-          produit: body.produit,
-          quantite: body.quantite.map((q) => -q),
-          travail_pour: user.travail_pour,
-          employe: user._id,
-          status_commande: 'Retour',
-          prix: -(product_return_type === 'half'
-            ? Number((total / 2).toFixed(2))
-            : total),
-          somme_encaisse: 0,
-          monnaie: 0,
-          employe_validate_id: user._id,
-        };
+						await produit.save();
+						body.produit[index] = {
+							...produit._doc,
+							productId: produit._id,
+						};
+					}
+				}
 
-        await venteQueries.setVente(venteData);
-      }
+				return sum;
+			};
 
-      const retournData = await retourQueries.setRetour({
-        ...body,
-        travail_pour: user.travail_pour,
-        employe: user._id,
-        remboursement:
-          product_return_type === 'half'
-            ? Number((total / 2).toFixed(2))
-            : total,
-        product_return_type: product_return_type,
-        confirm: ['full', 'half'].includes(product_return_type) ? true : false,
-      });
+			if (['full', 'half'].includes(product_return_type)) {
+				total = await updateProductStock();
 
-      req.session.retournData = retournData.result;
+				let venteData = {
+					produit: body.produit,
+					quantite: body.quantite.map((q) => -q),
+					travail_pour: user.travail_pour,
+					employe: user._id,
+					status_commande: 'Retour',
+					prix: -(product_return_type === 'half'
+						? Number((total / 2).toFixed(2))
+						: total),
+					somme_encaisse: 0,
+					monnaie: 0,
+					employe_validate_id: user._id,
+				};
 
-      res.redirect('retournerproduit');
-    } else {
-      res.redirect('emconnexion');
-    }
-  } catch (error) {
-    res.redirect(error);
-  }
+				await venteQueries.setVente(venteData);
+			}
+
+			const retournData = await retourQueries.setRetour({
+				...body,
+				travail_pour: user.travail_pour,
+				employe: user._id,
+				remboursement:
+					product_return_type === 'half'
+						? Number((total / 2).toFixed(2))
+						: total,
+				product_return_type: product_return_type,
+				confirm: ['full', 'half'].includes(product_return_type) ? true : false,
+			});
+
+			req.session.retournData = retournData.result;
+
+			res.redirect('retournerproduit');
+		} else {
+			res.redirect('emconnexion');
+		}
+	} catch (error) {
+		res.redirect(error);
+	}
 };
 
 exports.confirmBackProduct = async (req, res) => {
-  try {
-    if (!req.session.user) {
-      res.redirect('/emconnexion');
-      return;
-    }
+	try {
+		if (!req.session.user) {
+			res.redirect('/emconnexion');
+			return;
+		}
 
-    const backProductId = req.params.id;
+		const backProductId = req.params.id;
 
-    const { result: backProduct } = await retourQueries.getRetourById(
-      backProductId
-    );
+		const { result: backProduct } = await retourQueries.getRetourById(
+			backProductId
+		);
 
-    if (!backProduct) {
-      res.status(401).json({
-        etat: false,
-        message: 'Produit introuvable',
-      });
-      return;
-    }
+		if (!backProduct) {
+			res.status(401).json({
+				etat: false,
+				message: 'Produit introuvable',
+			});
+			return;
+		}
 
-    const { result: retour } = await retourQueries.updateRetour(
-      backProduct._id,
-      {
-        confirm: true,
-        confirm_by: req.session.user._id,
-      }
-    );
+		const { result: retour } = await retourQueries.updateRetour(
+			backProduct._id,
+			{
+				confirm: true,
+				confirm_by: req.session.user._id,
+			}
+		);
 
-    res.json({
-      etat: true,
-      message: 'Produit retourné avec succès',
-      data: retour,
-    });
-  } catch (error) {
-    res.redirect('/emconnexion');
-  }
+		res.json({
+			etat: true,
+			message: 'Produit retourné avec succès',
+			data: retour,
+		});
+	} catch (error) {
+		res.redirect('/emconnexion');
+	}
 };
 
 exports.listeRetour = async (req, res) => {
-  try {
-    const { result: retourProduits } = await retourQueries.getRetour({
-      travail_pour: req.session.user.travail_pour,
-      employe: req.session.user._id,
-    });
+	try {
+		const { result: retourProduits } = await retourQueries.getRetour({
+			travail_pour: req.session.user.travail_pour,
+			employe: req.session.user._id,
+		});
 
-    res.render('listeretournerproduit', {
-      user: req.session.user,
-      retourProduits,
-      type_retour_produit: TYPE_RETOUR_PRDUITS,
-    });
-  } catch (error) {}
+		res.render('listeretournerproduit', {
+			user: req.session.user,
+			retourProduits,
+			type_retour_produit: TYPE_RETOUR_PRDUITS,
+		});
+	} catch (error) {}
 };
 
 exports.getProductReturn = async (req, res) => {
-  try {
-    const code = req.params.code;
+	try {
+		const code = req.params.code;
 
-    const data = await retourQueries.getRetourByCode(
-      new RegExp(`${code.toLowerCase()}$`)
-    );
+		const data = await retourQueries.getRetourByCode(
+			new RegExp(`${code.toLowerCase()}$`)
+		);
 
-    if (!data.etat)
-      return res.status(404).json({ etat: false, message: 'Not found' });
+		if (!data.etat)
+			return res.status(404).json({ etat: false, message: 'Not found' });
 
-    const return_product = data.result[0];
+		const return_product = data.result[0];
 
-    if (return_product.product_return_type === 'tip') {
-      if (new Date().getTime() <= new Date(return_product.dateline).getTime()) {
-        return res.status(200).json({
-          etat: true,
-          message: `Ce code est valide jusqu'au ${new Date(
-            return_product.dateline
-          ).toLocaleString('fr-fr')}`,
-          data: return_product,
-        });
-      }
-    }
+		if (return_product.product_return_type === 'tip') {
+			if (new Date().getTime() <= new Date(return_product.dateline).getTime()) {
+				return res.status(200).json({
+					etat: true,
+					message: `Ce code est valide jusqu'au ${new Date(
+						return_product.dateline
+					).toLocaleString('fr-fr')}`,
+					data: return_product,
+				});
+			}
+		}
 
-    res.status(301).json({
-      etat: true,
-      message: `Ce code est expiré depuis le ${new Date(
-        return_product.createdAt
-      ).toLocaleString('fr-fr')}`,
-      code,
-    });
-  } catch (error) {
-    res.status(500).json({
-      etat: false,
-      message: error.message,
-    });
-  }
+		res.status(301).json({
+			etat: true,
+			message: `Ce code est expiré depuis le ${new Date(
+				return_product.createdAt
+			).toLocaleString('fr-fr')}`,
+			code,
+		});
+	} catch (error) {
+		res.status(500).json({
+			etat: false,
+			message: error.message,
+		});
+	}
 };
 
 exports.getProductsReturnValid = async (req, res) => {
-  try {
-    if (req.session.user) {
-      const productsReturn = await retourQueries.getRetour({
-        stock_return: false,
-        confirm: false,
-        dateline: {
-          $gte: new Date(),
-        },
-        product_return_type: 'tip',
-        travail_pour: req.session.user.travail_pour,
-      });
+	try {
+		if (req.session.user) {
+			const productsReturn = await retourQueries.getRetour({
+				stock_return: false,
+				confirm: false,
+				dateline: {
+					$gte: new Date(),
+				},
+				product_return_type: 'tip',
+				travail_pour: req.session.user.travail_pour,
+			});
 
-      const data = productsReturn?.result;
+			const data = productsReturn?.result;
 
-      const products = helperFormatReturnProducts(data);
+			const products = helperFormatReturnProducts(data);
 
-      res?.send({
-        data: products || [],
-      });
-    } else {
-      res.status(401).send({
-        message: 'Unauthorized',
-      });
-    }
-  } catch (err) {
-    res.status(500).send({
-      message: err.message,
-    });
-  }
+			res?.send({
+				data: products || [],
+			});
+		} else {
+			res.status(401).send({
+				message: 'Unauthorized',
+			});
+		}
+	} catch (err) {
+		res.status(500).send({
+			message: err.message,
+		});
+	}
 };
 
 exports.getProductReturnByQuery = async (req, res) => {
-  const user = req.session.user;
+	const user = req.session.user;
 
-  if (user) {
-    const travail_pour = user.travail_pour || user.id || user._id;
-    const { start, end, users } = formatQuery(req.query);
+	if (user) {
+		const travail_pour = user.travail_pour || user.id || user._id;
+		const { start, end, users } = formatQuery(req.query);
 
-    let filter = {
-      travail_pour: travail_pour,
-      product_return_type: 'tip',
-    };
+		let filter = {
+			travail_pour: travail_pour,
+			product_return_type: 'tip',
+		};
 
-    if (start) {
-      filter.dateline = {
-        $gte: start,
-      };
-    }
+		if (start) {
+			filter.dateline = {
+				$gte: start,
+			};
+		}
 
-    if (end) {
-      if (!filter.dateline) {
-        filter.dateline = {};
-      }
+		if (end) {
+			if (!filter.dateline) {
+				filter.dateline = {};
+			}
 
-      filter.dateline.$lte = end;
-    }
+			filter.dateline.$lte = end;
+		}
 
-    if (users?.length > 0) {
-      filter.employe = {
-        $in: users,
-      };
-    }
+		if (users?.length > 0) {
+			filter.employe = {
+				$in: users,
+			};
+		}
 
-    const productsReturnExpired = await retourQueries.getRetour({
-      $and: [
-        filter,
-        {
-          dateline: {
-            $lt: new Date(),
-          },
-        },
-      ],
-    });
+		const productsReturnExpired = await retourQueries.getRetour({
+			$and: [
+				filter,
+				{
+					dateline: {
+						$lt: new Date(),
+					},
+				},
+			],
+		});
 
-    const productsReturnPending = await retourQueries.getRetour({
-      travail_pour: travail_pour,
-      product_return_type: 'tip',
-      dateline: {
-        $gte: new Date(),
-      },
-      stock_return: false,
-      confirm: false,
-    });
+		const productsReturnPending = await retourQueries.getRetour({
+			travail_pour: travail_pour,
+			product_return_type: 'tip',
+			dateline: {
+				$gte: new Date(),
+			},
+			stock_return: false,
+			confirm: false,
+		});
 
-    res?.send({
-      data: {
-        tipsPending: productsReturnPending?.result || [],
-        tipsExpired: productsReturnExpired?.result || [],
-      },
-    });
-  } else {
-    res.status(401).send({
-      message: 'Unauthorized',
-    });
-  }
+		res?.send({
+			data: {
+				tipsPending: productsReturnPending?.result || [],
+				tipsExpired: productsReturnExpired?.result || [],
+			},
+		});
+	} else {
+		res.status(401).send({
+			message: 'Unauthorized',
+		});
+	}
 };
